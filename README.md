a482988e1484dcf8e28fef6c102c16db9bc23826
Stu <stumac1000@yahoo.com>
2024-03-27 16:55:53 -0700

    Create all field constraints

--- README.md
+++ README.md
@@ -4,29 +4,6 @@
 
 NPX WEBPACK --WATCH
 
-create a :valid and :invalid CSS pseudo class for styling.
-
-Add an 'e.preventdefault'
-
-email will be input type="email"
-country will be a select dropdown
-zip will be input type="text"
-password will be input type="text"
-password confirmation will also be input type="text"
-
-use 'checkvalidity()', 'reportvalidity()' and 'customvalidation'
-
-utilize 'onblur' to validate when leaving each field then if its invalid activate the 'oninput' event listener for active validation.
-
-follow 'constraint validation' webpage for event listening to country selection and zip regex.
-
-the password should have requirements written next to it and utilize 'oninput' for password strength.
-
-the password confirmation should have the 'onblur' event (when the user clicks away) to make sure it matches with the previous password field
-
 create aria labels. aria described by
-make sure tab index works properly
 
 make the high five animation upon success. Maybe a high five emoji that spans larger then backs off a little to represent a high five.
-
-make responsive design
--- src/index.js
+++ src/index.js
@@ -2,10 +2,103 @@ import './normalize.css';
 import './style.css';
 
 const email = document.querySelector('#email');
-const form = document.querySelector('form');
 
 email.addEventListener('input', (e) => {
-  console.log(form.checkValidity());
   console.log(email.reportValidity());
   console.log(email.validity);
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity('Please enter an email address');
+  } else {
+    email.setCustomValidity('');
+  }
+});
+
+document.querySelector('#country').addEventListener('change', checkZIP);
+document.querySelector('#zip').addEventListener('input', checkZIP);
+
+function checkZIP() {
+  const constraints = {
+    ch: [
+      '^(CH-)?\\d{4}$',
+      'Switzerland ZIPs must have exactly 4 digits: e.g. CH-1950 or 1950',
+    ],
+    fr: [
+      '^(F-)?\\d{5}$',
+      'France ZIPs must have exactly 5 digits: e.g. F-75012 or 75012',
+    ],
+    de: [
+      '^(D-)?\\d{5}$',
+      'Germany ZIPs must have exactly 5 digits: e.g. D-12345 or 12345',
+    ],
+    nl: [
+      '^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$',
+      'Netherland ZIPs must have exactly 4 digits, followed by 2 letters except SA, SD and SS',
+    ],
+  };
+
+  const country = document.querySelector('#country').value;
+
+  const ZIPField = document.querySelector('#zip');
+
+  const constraint = new RegExp(constraints[country][0], '');
+
+  if (constraint.test(ZIPField.value)) {
+    ZIPField.setCustomValidity('');
+  } else {
+    ZIPField.setCustomValidity(constraints[country][1]);
+  }
+}
+
+const pass = document.querySelector('#pass');
+
+pass.addEventListener('input', () => {
+  if (document.querySelector('.strength')) {
+    document.querySelector('.strength').remove();
+  }
+  const strength = document.createElement('span');
+  strength.classList.add('strength');
+
+  if (pass.value.length < 4) {
+    strength.innerText = 'weak';
+    strength.style.color = 'red';
+  } else if (pass.value.length < 7) {
+    strength.innerText = 'decent';
+    strength.style.color = 'orange';
+  } else if (pass.value.length < 10) {
+    strength.innerText = 'good';
+    strength.style.color = 'yellow';
+  } else {
+    strength.innerText = 'great!';
+    strength.style.color = 'green';
+  }
+  pass.parentElement.append(strength);
+});
+
+const passConfirm = document.querySelector('#pass-confirm');
+
+passConfirm.addEventListener('input', () => {
+  if (pass.value !== passConfirm.value) {
+    passConfirm.setCustomValidity('Passwords need to match');
+  } else {
+    passConfirm.setCustomValidity('');
+  }
+});
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  console.log(email.validity.valid);
+  if (email.value === '') {
+    email.setCustomValidity('Please enter an email address');
+    e.preventDefault();
+  } else if (document.querySelector('#zip').value === '') {
+    checkZIP();
+    e.preventDefault();
+  } else if (pass.value === '') {
+    pass.setCustomValidity('Please enter a password');
+    e.preventDefault();
+  } else if (passConfirm.value === '') {
+    passConfirm.setCustomValidity('Please confirm your password');
+    e.preventDefault();
+  }
 });
